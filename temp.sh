4dff70d18085596cef320b0308adff04b6cdb70e
Henry LETELLIER <henry.letellier@epitech.eu>
2022-04-06 16:32:57 +0200

    trying to fix bugs#A5

--- temp.sh
+++ temp.sh
@@ -5,3 +5,13 @@ find . -type f \( -name "*.o" -o -name "*.a" \) -delete
 find . -type f \( -name "*.exe" -o -name "*.so" \) -delete
 find . -type f \( -name "*.gcno" -o -name "*.gcda" -o -name "*.gcov" \) -delete
 find . -type f \( -name "tmp" \) -delete
+
+
+for FILE in *
+do
+    if grep -q "*~" $FILE
+    then
+        echo "::error file={FUck you},line={1},endLine={0},title={Unwanted file detected:}::{$FILE#L1}"
+        echo '::set-output name=SELECTED_COLOR::red'
+    fi
+done
