126f4bf4ce92fc7e608f7e10c5742276f647f64f
Henry LETELLIER <henry.letellier@epitech.eu>
2022-04-08 16:36:08 +0200

    updated the yml workflow

--- temp.sh
+++ temp.sh
@@ -19,3 +19,8 @@ done
 ['*~', '*-;', '#*', '#*#', '*#', '*.o', '*.a', '*.exe', '*.so', '*.gcno', '*.gcda', '*.gcov', 'tmp', "*pain_au_chocolat*" , "*pain_au_chocolat", "pain_au_chocolat*"]
 
 [ 'grep -q "*~" $FILE', 'grep -q "*-;" $FILE', 'grep -q "#*" $FILE', 'grep -q "#*#" $FILE', 'grep -q "*#" $FILE', 'grep -q "*.o" $FILE', 'grep -q "*.a" $FILE', 'grep -q "*.exe" $FILE', 'grep -q "*.so" $FILE', 'grep -q "*.gcno" $FILE', 'grep -q "*.gcda" $FILE', 'grep -q "*.gcov" $FILE', 'grep -q "tmp" $FILE', 'grep -q "*pain_au_chocolat*" $FILE', 'grep -q "*pain_au_chocolat" $FILE', 'grep -q "pain_au_chocolat*" $FILE']
+
+#!/bin/bash
+["*~", "*pain_au_chocolat*", "*#*", "*.exe", "*.sh", "*.so", "*.o", "*.gcda", "*.gcov", "tmp","*.a", "*-;", "*.gcno" ]
+
+find -name "*~" -o -name "*pain_au_chocolat*" -o -name "*#*" -o -name "*.exe" -o -name "*.sh" -o -name "*.so" -o -name "*.o" -o -name "*.gcda" -o -name "*.gcov" -o -name "tmp" -o -name "*.a" -o -name "*-;" -o -name "*.gcno" -type f,d
